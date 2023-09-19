64a66c859db4a78b4720b6bdd28cf4873b1997c2
Hoang Quoc Anh <102196398+akaisaiza@users.noreply.github.com>
2023-09-19 21:40:42 +0700

    create project

---
+++ SportifyShop_Be/SportifyShop_Be/src/main/java/fpt/edu/vn/SportifyShop_Be/SportifyShopBeApplication.java
@@ -0,0 +1,13 @@
+package fpt.edu.vn.SportifyShop_Be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportifyShopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportifyShopBeApplication.class, args);
+	}
+
+}
---
+++ SportifyShop_Be/SportifyShop_Be/src/test/java/fpt/edu/vn/SportifyShop_Be/SportifyShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.edu.vn.SportifyShop_Be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportifyShopBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
